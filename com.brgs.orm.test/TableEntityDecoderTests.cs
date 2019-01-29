eaee27ad6dc6f2a8b77670ac89fd098b2a641790
Luke Badgerow <luke@Lukes-MacBook-Pro.local>
2019-01-29 11:18:30 -0500

    there's a flakey test here I believe that it's related to the way the test runner is holding values by ref in the stack.

--- com.brgs.orm.test/TableEntityDecoderTests.cs
+++ com.brgs.orm.test/TableEntityDecoderTests.cs
@@ -15,27 +15,23 @@ namespace com.brgs.orm.test
         [Fact]
         public void DoesConvertITableEntityIntoDomainObject()
         {
+            var river = new River()
+            {
+               Name = "GAULEY RIVER BELOW SUMMERSVILLE DAM, WV",
+               RiverId = "03189600",
+               State = "West Virginia",
+               StateCode = "WV",
+               Srs = "EPSG:4326",
+               Latitude = "38.2151103",
+               Longitude = "-80.8881536",
+               Id="Gauley|03189600"
+            };
+            var helper = new AzureFormatHelper("pizza");
+            var entity = helper.BuildTableEntity(river);
 
-            //var river = new River()
-            //{
-            //    Name = "GAULEY RIVER BELOW SUMMERSVILLE DAM, WV",
-            //    RiverId = "03189600",
-            //    State = "West Virginia",
-            //    StateCode = "WV",
-            //    Srs = "EPSG:4326",
-            //    Latitude = "38.2151103",
-            //    Longitude = "-80.8881536",
-            //    Id="Gauley|03189600"
-            //};
-            //var helper = new AzureFormatHelper("pizza");
-            //var entity = helper.BuildTableEntity(river);
-
-            //var testVal = (River)AzureFormatHelper.RecastEntity(entity, typeof(River));
-            //Assert.Equal(river.Name, testVal.Name);
-
-            var helper = new AzureFormatHelper();
-            Assert.Equal(1, 1);
-
+            var testVal = (River)AzureFormatHelper.RecastEntity(entity, typeof(River));
+            Assert.Equal(river.Name, testVal.Name);
+            Assert.Equal(river.RiverId, testVal.RiverId);
         }        
     }
 }   
