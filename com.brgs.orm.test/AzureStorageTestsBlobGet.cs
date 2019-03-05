b72ca498643932238258548911e943af6261f4b1
Badgerow <badgerow.luke@gmail.com>
2019-03-05 13:09:52 -0500

    here's a completed working test (37% coverage)

--- com.brgs.orm.test/AzureStorageTestsBlobGet.cs
+++ com.brgs.orm.test/AzureStorageTestsBlobGet.cs
@@ -10,6 +10,10 @@ using com.brgs.orm;
 using Microsoft.WindowsAzure.Storage.Auth;
 using System.Reflection;
 using System.Linq;
+using System.IO;
+using System.Text;
+using System.Runtime.Serialization.Formatters.Binary;
+using Newtonsoft.Json;
 
 namespace com.brgs.orm.test
 {
@@ -23,19 +27,28 @@ namespace com.brgs.orm.test
             var blobContainer = new Mock<CloudBlobContainer>(new Uri("https://www.google.com"));
 
             var blob = new Mock<CloudBlob>(new Uri("https://waterfinder.blob.core.windows.net/data"));
-            blob.Setup
-
-            blobContainer.Setup(bc => bc.GetBlobReference(It.IsAny<string>())).Returns(blob.Object);
-            blobClient.Setup(b => b.GetContainerReference(It.IsAny<string>())).Returns(blobContainer.Object);
-
-            acc.Setup(c => c.CreateCloudBlobClient()).Returns(blobClient.Object);
-
-            var fac = new AzureStorageFactory(acc.Object)
-            {
-                CollectionName ="PIZZA"
+            var riverList = new List<River>(){
+                new River()
             };
-            var stuff = fac.Get<List<River>>("TACOS");
-            Assert.NotEmpty(stuff);
+            var listString = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(riverList));
+
+            using (var val = new MemoryStream(listString))
+            {
+                blob.Setup(b =>b.OpenReadAsync()).ReturnsAsync(val);
+                blobContainer.Setup(bc => bc.GetBlobReference(It.IsAny<string>())).Returns(blob.Object);
+                blobClient.Setup(b => b.GetContainerReference(It.IsAny<string>())).Returns(blobContainer.Object);
+
+                acc.Setup(c => c.CreateCloudBlobClient()).Returns(blobClient.Object);
+
+                var fac = new AzureStorageFactory(acc.Object)
+                {
+                    CollectionName ="PIZZA"
+                };
+                var stuff = fac.Get<List<River>>("TACOS");
+                Assert.NotEmpty(stuff);
+            }
+            
+
         }
     }
 }
