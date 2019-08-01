ae21a43700e6c9f6dc078d2fff5493489e843b1d
Luke Badgerow <Luke.Badgerow@insight.com>
2019-08-01 13:33:17 -0400

    a little more built out batch processing

--- com.brgs.orm/Azure/AzureTableBuilder.cs
+++ com.brgs.orm/Azure/AzureTableBuilder.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using com.brgs.orm.Azure.helpers;
 using Microsoft.WindowsAzure.Storage;
@@ -71,15 +72,84 @@ namespace com.brgs.orm.Azure
             }            
         }
         ///<summary>each individual batch needs to be less than or equal to 100</summary>
-        public async Task<IList<TableResult>> PostBatchAsync<T>(IEnumerable<T> records)
+        public async Task<int> PostBatchAsync<T>(IEnumerable<T> records)
         {
-            TableBatchOperation batch = new TableBatchOperation();
 
             var tableClient = account.CreateCloudTableClient();
             var table = tableClient.GetTableReference(Collection);
-            var results = await table.ExecuteBatchAsync(batch);
 
-            return results;
+            var didCreate = await table.CreateIfNotExistsAsync();
+
+            IList<TableResult> result = null;
+
+            if (records.Count() <= 100)
+            {
+                var batch = BuildBatch<T>(records);
+                result = await table.ExecuteBatchAsync(batch);
+                return result.Count;
+            }
+            else
+            {
+                int recordCount = 0;
+                do
+                {
+                    var partial = records.Skip(recordCount).Take(100);
+                    var batch = BuildBatch(partial);
+                    result = await table.ExecuteBatchAsync(batch);
+                    var val = partial.Count();
+                    recordCount = recordCount + partial.Count();;
+
+                } while (recordCount < records.Count());
+                return recordCount;
+            }
+        }
+
+        private TableBatchOperation BuildBatch<T>(IEnumerable<T> records)
+        {
+            TableBatchOperation batch = new TableBatchOperation();
+            foreach (var record in records)
+            {
+                if (record is ITableEntity)
+                {
+                    if(string.IsNullOrEmpty((record as ITableEntity).PartitionKey))
+                    {
+                        (record as ITableEntity).PartitionKey = to.PartitionKey;
+                    }
+                    batch.InsertOrReplace((ITableEntity)record);
+                }
+                else
+                {
+                    var obj = BuildTableEntity(record);
+                    batch.InsertOrReplace((ITableEntity)obj);
+                }
+            }
+            return batch;
+        }
+        public void DeleteBatchAsync<T>(IEnumerable<T> records)
+        {
+
+            var tableClient = account.CreateCloudTableClient();            
+            var table = tableClient.GetTableReference(to.CollectionName);
+            Action<TableBatchOperation, ITableEntity> batchOperationAction = null;
+            batchOperationAction = (bo, entity) => bo.Delete(entity);
+            TableBatchOperation batch = new TableBatchOperation();
+            var tasks = new List<Task<IList<TableResult>>>();
+            var entitiesOffset = 0;
+            while (entitiesOffset < records?.Count())
+            {
+                var entitiesToAdd = records.Skip(entitiesOffset).Take(100).ToList();
+                entitiesOffset += entitiesToAdd.Count;
+
+                TableBatchOperation batchOperation = new TableBatchOperation();
+
+                entitiesToAdd.ForEach(entity => batchOperationAction(batchOperation, (ITableEntity)entity));
+
+                tasks.Add(table.ExecuteBatchAsync(batchOperation));
+            }
+
+            IList<TableResult>[] results =  Task.WhenAll(tasks).ConfigureAwait(false).GetAwaiter().GetResult();            
+
+
         }
     }
 }
