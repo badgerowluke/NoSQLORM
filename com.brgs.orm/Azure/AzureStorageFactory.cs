bb79cc9bc3cfaad10ae2459e9af191b05dd92d8c
Luke Badgerow <Luke.Badgerow@insight.com>
2019-08-02 15:58:12 -0400

    here's getting from Table API with lambda/LINQ statements. pretty limited, but it does it

--- com.brgs.orm/Azure/AzureStorageFactory.cs
+++ com.brgs.orm/Azure/AzureStorageFactory.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using Microsoft.WindowsAzure.Storage.Table;
 using com.brgs.orm.Azure.helpers;
 using System;
+using System.Linq.Expressions;
 
 namespace com.brgs.orm.Azure
 {
@@ -11,24 +12,42 @@ namespace com.brgs.orm.Azure
         private ICloudStorageAccount account;
         public string CollectionName { get; set; }
         public string PartitionKey { get; set; }
+        private readonly Interegator _parser;
         public AzureStorageFactory(ICloudStorageAccount acc)
         {
             account = acc;
+            _parser = new Interegator();
 
         }
-        public void Register(string collection, string partition)
-        {
 
-
-        }
         public T Get<T>( string blobName)
         {
             return new AzureBlobBuilder(account)
                                 .GetAsync<T>(CollectionName, blobName).Result;
         }
+        public IEnumerable<T> Get<T>(Expression<Func<T,bool>> predicate)
+        {
+
+            if(string.IsNullOrEmpty(CollectionName))
+            {
+                throw new ArgumentException("we need to have a collection");
+            }
+            var query = new TableQuery();
+            var filter = _parser.BuildQueryFilter(predicate);
+            query.Where(filter);
+
+            return new AzureTableBuilder(account)
+                                .GetAsync<List<T>>(query, CollectionName).GetAwaiter().GetResult();
+
+
+        }
 
         public T Get<T>(TableQuery query) 
         {
+            if(string.IsNullOrEmpty(CollectionName))
+            {
+                throw new ArgumentException("we need to have a collection");
+            }            
             return new AzureTableBuilder(account)
                                 .GetAsync<T>(query, CollectionName).Result;
         }
